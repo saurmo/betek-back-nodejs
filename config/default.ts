23af812b2898cb4b9bf5d44ad1023f1a24d7c0a8
saurmo0108@gmail.com <saurmo0108@gmail.com>
2024-10-04 07:45:11 -0500

    feat: configuracion base de datos

---
+++ config/default.ts
@@ -0,0 +1,8 @@
+// en un nivel más avanzado usamos las variables de entorno
+export default {
+  HOST: "localhost",
+  PORT: 3306,
+  USER: "root",
+  PASSWORD: "saurmo12", 
+  DATABASE: "tienda_virtual",
+};
