f92636dd19da5ab14835334b759af0d0fc57ae8c
saurmo0108@gmail.com <saurmo0108@gmail.com>
2024-10-23 15:21:13 +0200

    feat: router de categorias

--- src/infrastructure/modules/api-rest/routers/categorias.router.ts
+++ src/infrastructure/modules/api-rest/routers/categorias.router.ts
@@ -1,32 +1,71 @@
 import Express from "express";
+import { CategoriaController } from "../../../../application/categoria.controller";
 
 // Cual es el objetivo:
 // Crear las rutas que se van a usar de categorias
 export const categoriasRoutes = () => {
   const router = Express.Router();
 
+  const ctrl = new CategoriaController();
+
   router.post("/categorias", (req, res) => {
-    res.send("Post categorias");
+    const payload = req.body;
+    // Resolver la promesa con then-catch del controlador
+    ctrl
+      .agregar(payload)
+      .then((result) => {
+        const status = result.ok === true ? 200 : 400;
+        res.status(status).send(result);
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
   });
 
   router.put("/categorias", (req, res) => {
-    res.send("Put categorias");
+    const payload = req.body;
+    ctrl
+      .actualizar(payload)
+      .then((result) => {
+        const status = result.ok === true ? 200 : 400;
+        res.status(status).send(result);
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
   });
 
   // ASYNC - AWAIT
   router.get("/categorias", async (_, res) => {
-    res.send("GET CATEGORIAS");
+    try {
+      const result = await ctrl.obtener();
+      res.send(result);
+    } catch (error) {
+      res.status(500).send(error);
+    }
   });
 
   // parametro dinamico /:id
-  router.get("/categorias/:id", (req, res) => {
-    const id = req.params.id;
-    res.send(`Get one categoria ${id}`);
+  router.get("/categorias/:id", async (req, res) => {
+    try {
+      const id = req.params.id;
+      const result = await ctrl.obtenerPorId(id);
+      const status = result.ok === true ? 200 : 404;
+      res.status(status).send(result);
+    } catch (error) {
+      res.status(500).send(error);
+    }
   });
 
-  router.delete("/categorias/:id", (req, res) => {
-    const id = req.params.id;
-    res.send(`delete one productos ${id}`);
+  router.delete("/categorias/:id", async (req, res) => {
+    try {
+      const id = req.params.id;
+      const result = await ctrl.eliminar(id);
+      const status = result.ok === true ? 200 : 400;
+      res.status(status).send(result);
+    } catch (error) {
+      res.status(500).send(error);
+    }
   });
 
   return router;
